6054084c349d646b1050314bbb7c349148da3cd3
Chinnu <saicharan05015@gmail.com>
2026-01-26 22:12:02 +0530

    Routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 
 
-app.use("/hii",(req,res)=>{
-    res.send("hello this is express from node_modules");
+app.get("/Get",(req,res)=>{
+    res.send({name:"saicharan",class:"B.Tech"})
+})
+
+app.post("/Post",(req,res)=>{
+    res.send("hello user your data put in the database");
+
+})
+
+app.delete("/Delete",(req,res)=>{
+    res.send("hello user your sucessfully deleted")
 
 })
 
 app.listen(3000,()=>{
     console.log("server is running on the port number 3000")
 
-});
+})
