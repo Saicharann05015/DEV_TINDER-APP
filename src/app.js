5da689906bf499090e4af64341c09bb82729e92c
Chinnu <saicharan05015@gmail.com>
2026-01-27 14:19:19 +0530

    dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,45 @@ const express = require("express");
 const app = express();
 
 
-app.get("/Get",(req,res)=>{
-    res.send({name:"saicharan",class:"B.Tech"})
+
+// params 
+
+app.get("/product/:id/review/:rid",(req,res)=>{
+    let {id,rid} = (req.params)
+    res.send(`hello this product so & so ${id} ${rid}` )
+
 })
+
+app.get("/value",(req,res)=>{
+  console.log(req.query);
+  res.send("hello this is query params")
+})
+
+
+app.get("/product/:id",(req,res)=>{
+    let {id} = req.params;
+    let {en,val} = req.query;
+    res.send(`this is the params ${id}, this is the query ${en,val}`);
+
+})
+
+
+
+
+
+
+
+
+
+
+// app.get("/log-in",(req,res)=>{
+//     res.send("login Page")
+
+// })
+
+// app.get("/Get",(req,res)=>{
+//     res.send({name:"saicharan",class:"B.Tech"})
+// })
 
 app.post("/Post",(req,res)=>{
     res.send("hello user your data put in the database");
